45a5feccd6982e054ec1fa21af66fe0220e56640
alanferreiradasilva <3091682+alanferreiradasilva@users.noreply.github.com>
2026-01-05 00:01:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navbar from './components/navbas/Navbar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-[#0EA8F2]">
+    <Navbar />
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+/* Estilos personalizados adicionais, se necessário */
+</style>
---
+++ src/views/ExchangeView.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center bg-[#FFFFFF] rounded-4xl p-10 shadow-lg">
+        <h1 class="text-5xl lg:text-6xl font-bold text-[#28215C] leading-tight">
+            Exchange View
+          </h1>
+      </div>
+    </section>
+
+    
+</template>
